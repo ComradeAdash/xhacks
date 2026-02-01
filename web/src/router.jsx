01841f948d5545350bea8a8e4882ce43ae7d66c7
Adam Siergiej <adamsiergiej25@gmail.com>
2026-01-31 19:39:29 -0800

    added profile pictures

--- web/src/router.jsx
+++ web/src/router.jsx
@@ -2,9 +2,11 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Authentication from "./authentication";
 import VerifyEmail from "./Verifyemail";
+import Profile from "./Profile";
 
 export const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/auth", element: <Authentication /> },
-  { path: "/verify", element: <VerifyEmail /> }
+  { path: "/verify", element: <VerifyEmail /> },
+  { path: "/profile", element: <Profile /> }
 ]);
